63a23c5728c38524e21a86b4d81737f5763303c8
Nazar <nz.shevchuk.2002@gmail.com>
2023-09-18 20:27:56 +0300

    server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { CanvasModel } from "./canvas";
 import { Castomizer, Home } from "./pages";
 
 export const App = () => {
-
+  console.log('test')
   return (
     <main className="app transition-all ease-in">
       <Home />
